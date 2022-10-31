31819e452eaa288d2944af6a223671fd0bc6b58a
linharesrocha <guilhermelinharesr@gmail.com>
2022-10-31 09:00:33 -0300

    login no aton no .env

--- mordomo/setup.py
+++ mordomo/setup.py
@@ -5,26 +5,34 @@ from time import sleep
 from dotenv import load_dotenv
 
 # Load Variables
-# env_path = Path('..') / 'C:\workspace\cadastro-aton\.env'
-# load_dotenv(dotenv_path=env_path)
-# login = os.environ['LOGIN_USER']
-# senha = os.environ['LOGIN_PASS']
-login = ''
-senha = ''
+env_path = Path('..') / 'C:\workspace\cadastro-aton\.env'
+load_dotenv(dotenv_path=env_path)
+login = os.environ['LOGIN_USER']
+senha = os.environ['LOGIN_PASS']
+
+move_aton_user_x = int(os.environ['MOVE_ATON_USER_X'])
+move_aton_user_y = int(os.environ['MOVE_ATON_USER_Y'])
+
+move_aton_pass_x = int(os.environ['MOVE_ATON_PASS_X'])
+move_aton_pass_y = int(os.environ['MOVE_ATON_PASS_Y'])
+
+move_aton_button_x = int(os.environ['MOVE_ATON_BUTTON_X'])
+move_aton_button_y = int(os.environ['MOVE_ATON_BUTTON_Y'])
+
 
 def login_aton():
     # Campo de Usuário
-    pg.moveTo(1583, 532)
+    pg.moveTo(move_aton_user_x, move_aton_user_y)
     pg.click()
     pg.typewrite(login)
 
     # Campo de Senha
-    pg.moveTo(1606, 563)
+    pg.moveTo(move_aton_pass_x, move_aton_pass_y)
     pg.click()
     pg.typewrite(senha)
 
     # Botão de Login
-    pg.moveTo(1550, 601)
+    pg.moveTo(move_aton_button_x, move_aton_button_y)
     pg.doubleClick()
 
     sleep(6)
