4dd38a27f97bd302db96f94c3cea558f0831d377
linharesrocha <guilhermelinharesr@gmail.com>
2022-12-14 13:16:52 -0300

    completando

--- mordomo/programas/sql_server_slack_pedidos_diarios.py
+++ mordomo/programas/sql_server_slack_pedidos_diarios.py
@@ -18,7 +18,7 @@ old_d1 = old_d1.strftime("%d-%m-%Y")
 date_today = today + timedelta(1)
 d1 = date_today.strftime("%d-%m-%Y")
 
-# warnings.filterwarnings('ignore')
+warnings.filterwarnings('ignore')
 
 lista_marketplaces = [
     'NETSHOES',
@@ -38,7 +38,12 @@ lista_marketplaces = [
 
 lista_quantidade_total = []
 lista_valores_total = []
-
+lista_quantidade_total_cnpj_madz = []
+lista_quantidade_total_cnpj_leal = []
+lista_quantidade_total_cnpj_pisste = []
+lista_valores_total_cnpj_madz = []
+lista_valores_total_cnpj_leal = []
+lista_valores_total_cnpj_pisste = []
 
 # SLACK SETUP
 env_path = Path('.') / '.env'
@@ -46,20 +51,19 @@ load_dotenv(dotenv_path=env_path)
 app = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 env_path_sql = Path('.') / '.env-sql'
 load_dotenv(dotenv_path=env_path_sql)
-DATABASE=os.environ['DATABASE']
-UID=os.environ['UID']
-PWD=os.environ['PWD']
+DATABASE = os.environ['DATABASE']
+UID = os.environ['UID']
+PWD = os.environ['PWD']
 
 dados_conexao = ("Driver={SQL Server};"
-            "Server=erp.ambarxcall.com.br;"
-            "Database="+DATABASE+";"
-            "UID="+UID+";"
-            "PWD="+PWD+";")
+                 "Server=erp.ambarxcall.com.br;"
+                 "Database=" + DATABASE + ";"
+                                          "UID=" + UID + ";"
+                                                         "PWD=" + PWD + ";")
 
 conexao = pyodbc.connect(dados_conexao)
 cursor = conexao.cursor()
 print("Conexão com o Banco de Dados Bem Sucedida!")
-
 
 comando = f'''
     SELECT PEDIDO, EMPRESA, DATA, ORIGEM, VENDEDOR, TOTAL_PEDIDO
@@ -76,8 +80,7 @@ quantidade_pedidos_total = data.shape[0]
 pedidos_total = round(data['TOTAL_PEDIDO'].sum(), 2)
 
 
-str_start = f". " \
-            f"\n======================================" \
+str_start = f"\n======================================" \
             f"\n{d1}" \
             f"\n{d1}" \
             f"\n{d1}" \
@@ -85,59 +88,77 @@ str_start = f". " \
             f"\n======================================"
 app.chat_postMessage(channel='tendencias-test', text=str_start)
 
-
-
 for marketplace in lista_marketplaces:
     data_marketplace = data[data['VENDEDOR'].str.contains(marketplace, na=False)]
     data_marketplace_madz = data_marketplace.loc[data_marketplace['EMPRESA'] == 1]
-    data_marketplace_leal =  data_marketplace.loc[data_marketplace['EMPRESA'] == 2]
+    data_marketplace_leal = data_marketplace.loc[data_marketplace['EMPRESA'] == 2]
     data_marketplace_pisste = data_marketplace.loc[data_marketplace['EMPRESA'] == 3]
 
     # MADZ
     quantidade_pedidos_total_mktp_madz = data_marketplace_madz.shape[0]
     pedidos_total_mktp_madz = round(data_marketplace_madz['TOTAL_PEDIDO'].sum(), 2)
+    lista_quantidade_total_cnpj_madz.append(quantidade_pedidos_total_mktp_madz)
+    lista_valores_total_cnpj_madz.append(pedidos_total_mktp_madz)
 
     # LEAL
     quantidade_pedidos_total_mktp_leal = data_marketplace_leal.shape[0]
     pedidos_total_mktp_leal = round(data_marketplace_leal['TOTAL_PEDIDO'].sum(), 2)
+    lista_quantidade_total_cnpj_leal.append(quantidade_pedidos_total_mktp_leal)
+    lista_valores_total_cnpj_leal.append(pedidos_total_mktp_leal)
 
     # PISSTE
     quantidade_pedidos_total_mktp_pisste = data_marketplace_pisste.shape[0]
     pedidos_total_mktp_pisste = round(data_marketplace_pisste['TOTAL_PEDIDO'].sum(), 2)
+    lista_quantidade_total_cnpj_pisste.append(quantidade_pedidos_total_mktp_pisste)
+    lista_valores_total_cnpj_pisste.append(pedidos_total_mktp_pisste)
 
     # SLACK
     if quantidade_pedidos_total_mktp_madz > 0:
         str_1 = f"{marketplace} MADZ" \
-                f"\nQuantidade Pedidos = {str(quantidade_pedidos_total_mktp_madz)}" \
-                f"\nValor Total Pedido = R${str(pedidos_total_mktp_madz)}" \
-                f"\n. " \
-
+                f"\nQntd:  {str(quantidade_pedidos_total_mktp_madz)}" \
+                f"\nValor:  R${str(pedidos_total_mktp_madz)}" \
+                f"\n. "
         app.chat_postMessage(channel='tendencias-test', text=str_1)
 
     if quantidade_pedidos_total_mktp_leal > 0:
         str_2 = f"{marketplace} LEAL" \
-                f"\nQuantidade Pedidos = {str(quantidade_pedidos_total_mktp_leal)}" \
-                f"\nValor Total Pedido = R${str(pedidos_total_mktp_leal)}" \
-                f"\n. " \
-
+                f"\nQntd:  {str(quantidade_pedidos_total_mktp_leal)}" \
+                f"\nValor:  R${str(pedidos_total_mktp_leal)}" \
+                f"\n. "
         app.chat_postMessage(channel='tendencias-test', text=str_2)
 
     if quantidade_pedidos_total_mktp_pisste > 0:
         str_3 = f"{marketplace} PISSTE" \
-                f"\nQuantidade Pedidos = {str(quantidade_pedidos_total_mktp_pisste)}" \
-                f"\nValor Total Pedido = R${str(pedidos_total_mktp_pisste)}" \
-                f"\n. " \
-
+                f"\nQntd:  {str(quantidade_pedidos_total_mktp_pisste)}" \
+                f"\nValor:  R${str(pedidos_total_mktp_pisste)}" \
+                f"\n. "
         app.chat_postMessage(channel='tendencias-test', text=str_3)
 
-
 # Relatório em cada CNPJ
+qntd_pedidos_madz = sum(lista_quantidade_total_cnpj_madz)
+qntd_pedidos_leal = sum(lista_quantidade_total_cnpj_leal)
+qntd_pedidos_pisste = sum(lista_quantidade_total_cnpj_pisste)
+qntd_vendas_madz = round(sum(lista_valores_total_cnpj_madz),2)
+qntd_vendas_leal = round(sum(lista_valores_total_cnpj_leal),2)
+qntd_vendas_pisste = round(sum(lista_valores_total_cnpj_pisste),2)
 
 
-# Somar Valores Três CNPJ
-str_final = f"." \
-            f"\n======================================" \
-            f"\nQuantidade de Pedidos Total: {str(quantidade_pedidos_total)}" \
-            f"\nQuantidade de Valor Total: R${str(float(pedidos_total))}" \
+str_cnpj = f"======================================" \
+           f"\n SOMA:" \
+           f"\n." \
+           f"\n MADZ:  {qntd_pedidos_madz}" \
+           f"\n LEAL:  {qntd_pedidos_leal}" \
+           f"\n PISSTE:  {qntd_pedidos_pisste}" \
+           f"\n ." \
+           f"\n MADZ:  R${qntd_vendas_madz}" \
+           f"\n LEAL:  R${qntd_vendas_leal}" \
+           f"\n PISSTE:  R${qntd_vendas_pisste}" \
+
+    # Somar Valores Três CNPJ
+str_final = f"\n======================================" \
+            f"\nQntd Pedidos Totais:  {str(quantidade_pedidos_total)}" \
+            f"\nValor Pedidos Totais:  R${str(float(pedidos_total))}" \
             f"\n======================================"
+
+app.chat_postMessage(channel='tendencias-test', text= str_cnpj)
 app.chat_postMessage(channel='tendencias-test', text=str_final)
