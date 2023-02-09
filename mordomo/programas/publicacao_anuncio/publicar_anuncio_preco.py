17f921d3463774c2a02cfcf7ec09f6b22ac31c5d
linharesrocha <guilhermelinharesr@gmail.com>
2023-02-09 12:13:50 -0300

    automacao preco de e preco por

--- mordomo/programas/publicacao_anuncio/publicar_anuncio_preco.py
+++ mordomo/programas/publicacao_anuncio/publicar_anuncio_preco.py
@@ -58,12 +58,48 @@ df_publica_produto = pd.read_sql(comando, conexao)
 df_planilha_precos = pd.read_excel('C:\workspace\cadastro-aton\mordomo\programas\excel\Envios_Magalu_Madz_Pisste_Leal.xlsx')
 df_publica_produto_com_precos_novos = df_publica_produto.merge(df_planilha_precos, on='CODID')
 
+# Renomeando DE E POR - BUG
+columns_name = {'PRECO_DE':'AUX', 'PRECO_POR':'PRECO_DE'}
+df_publica_produto_com_precos_novos.rename(columns=columns_name, inplace=True)
+df_publica_produto_com_precos_novos.rename(columns={'AUX':'PRECO_POR'}, inplace=True)
+
 # Alterarando ponto para virgula, para gravação de dados no Banco
-colunas_ponto = ['PRECO_DE_ANTIGO', 'PRECO_POR_ANTIGO', 'PRECO_POR', 'PRECO_DE']
+colunas_ponto = ['PRECO_POR', 'PRECO_DE']
+
 for col in colunas_ponto:
     # Converte o ponto para vírgula e com apenas duas casas decimais
-    df_publica_produto_com_precos_novos[col] = df_publica_produto_com_precos_novos[col].apply(lambda x: "{:,.2f}".format(x).replace('.', ','))
+    df_publica_produto_com_precos_novos[col] = df_publica_produto_com_precos_novos[col].apply(lambda x: "{:,.1f}".format(x))
 
 for i in range(len(df_publica_produto_com_precos_novos)):
+    # Coletando as variáveis
+    preco_de_antigo = df_publica_produto_com_precos_novos['PRECO_DE_ANTIGO'][i]
+    preco_por_antigo = df_publica_produto_com_precos_novos['PRECO_POR_ANTIGO'][i]
+    preco_de_novo = df_publica_produto_com_precos_novos['PRECO_DE'][i]
+    preco_por_novo = df_publica_produto_com_precos_novos['PRECO_POR'][i]
+    codid = df_publica_produto_com_precos_novos['CODID'][i]
     
-    break
+    # Printando informações
+    print(f'\n{str(i)}/{str(len(df_publica_produto_com_precos_novos))} - CODID:{codid}')
+    print(f'Preço De Antigo: {preco_de_antigo}\nPreco De Novo:{preco_de_novo}\n\nPreço Por Antigo:{preco_por_antigo}\nPreco Por Novo:{preco_por_novo}')
+    
+    # Substitui o PRECO DE
+    comando = f'''
+    UPDATE PUBLICA_PRODUTO
+    SET VALOR1 = REPLACE(VALOR1, '{preco_de_antigo}', '{preco_de_novo}')
+    WHERE CODID = '{codid}'
+    AND DATATH > {today_format_Y}
+    '''
+    cursor.execute(comando)
+    conexao.commit()
+    
+    # Substitui o PRECO POR
+    comando = f'''
+    UPDATE PUBLICA_PRODUTO
+    SET VALOR2 = REPLACE(VALOR2, '{preco_por_antigo}', '{preco_por_novo}')
+    WHERE CODID = '{codid}'
+    AND DATATH > {today_format_Y}
+    '''
+    cursor.execute(comando)
+    conexao.commit()
+    
+del conexao
