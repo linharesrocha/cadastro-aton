d0203120a4c9ef04648603693a8698a6afd8f67d
linharesrocha <guilhermelinharesr@gmail.com>
2023-02-15 09:24:02 -0300

    traz os pais

--- mordomo/programas/publicacao_anuncio/corrige_publicacao_id_nao_vinculado.py
+++ mordomo/programas/publicacao_anuncio/corrige_publicacao_id_nao_vinculado.py
@@ -25,20 +25,26 @@ def check_date_format(date_string):
         return False
     
 while True:
-    today_format_Y = input('\nQual a data que foi inserido as publicações?: ')
+    today_format_Y = input('\nQual a data que foi inserido as publicações? (DD/MM): ')
     if check_date_format(today_format_Y):
         today_format_Y = today_format_Y + '-2023'
         break
     else:
         print(f'{today_format_Y} não está no formato correto: DIA-MÊS. Por favor, tente novamente.')
    
-
-# Pega todos os produtos que deram erro 'ID Variação não vinculado ao Produto Pai .'
+while True:
+    error_input = input('\nQual erro você deseja pesquisar?: ')
+    if len(error_input) > 3:
+        error_input = str('%' + error_input + '%')
+        break
+    else:
+        print('Descreva mais!')
+# Pega todos os produtos que deram erro
 comando = f'''
 SELECT AUTOID, AUTOIDPAI, STATUSCODE, RETORNOSTR, SKURETORNO, FLAG, ERRO_API
 FROM PUBLICA_PRODUTO
 WHERE DATATH > '{today_format_Y}'
-AND ERRO_API LIKE 'ID Vari%'
+AND ERRO_API LIKE '{error_input}'
 AND FLAG = '1'
 '''
 
@@ -54,23 +60,37 @@ autoidpai_set.discard(0)
 
 # Confirma se já está na tela?
 while True:
-    anwser = input('Você já está na tela de Publicação de Anúncios? (S): ').lower()
+    anwser = input('\nVocê já está na tela de Produtos Selecionados? (S): ').lower()
     if anwser == 'sim' or anwser == 's':
         break
 
 # Faz a publicação dos produtos com variações primeiro
 print(Fore.LIGHTBLUE_EX, f'\nQuantidades de AUTOIDPAI: {str(len(autoidpai_set))}')
-for autoidpai in autoidpai_set:
+for indice, autoidpai in enumerate(autoidpai_set):
+    # Traz os filhos
     comando = f'''
     UPDATE PUBLICA_PRODUTO
     SET FLAG = '0'
     WHERE DATATH > '{today_format_Y}'
-    AND ERRO_API LIKE 'ID Vari%'
+    AND ERRO_API LIKE '{error_input}'
     AND AUTOIDPAI = '{autoidpai}'
     '''
     
     cursor.execute(comando)
     conexao.commit()
+    
+    # Traz os pais
+    comando = f'''
+    SET FLAG = '0'
+    WHERE DATATH > '{today_format_Y}'
+    AND ERRO_API LIKE '{error_input}'
+    AND AUTOID = '{autoidpai}'
+    '''
+    
+    cursor.execute(comando)
+    conexao.commit()
+    
+    print(Fore.LIGHTBLUE_EX, f'\n{str(indice)}/{str(len(autoidpai_set))} - AUTOIDPAI:{autoidpai}')
     
     # Aguarda um segundo antes de consultar
     sleep(1)
@@ -113,15 +133,18 @@ for autoidpai in autoidpai_set:
     print(Fore.LIGHTBLUE_EX, f'*' * 50)
     
 print(Fore.LIGHTBLUE_EX, f'\nQuantidades de AUTOID: {str(len(autoidsimples_list))}')
+
 # Faz a publicação dos produtos simples
-for autoid in autoidsimples_list:
+for indice, autoid in enumerate(autoidsimples_list):
     comando = f'''
     UPDATE PUBLICA_PRODUTO
     SET FLAG = '0'
     WHERE DATATH > '{today_format_Y}'
-    AND ERRO_API LIKE 'ID Vari%'
+    AND ERRO_API LIKE '{error_input}'
     AND AUTOID = '{autoid}'
     '''
+    
+    print(Fore.LIGHTBLUE_EX, f'\n{str(indice)}/{str(len(autoidsimples_list))} - AUTOIDPAI:{autoidpai}')
     
     cursor.execute(comando)
     conexao.commit()
