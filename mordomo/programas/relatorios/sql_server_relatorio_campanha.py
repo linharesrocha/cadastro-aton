5a0b37b1bc5aebfeac8f5e6903cde896123e9362
linharesrocha <guilhermelinharesr@gmail.com>
2023-03-09 15:21:51 -0300

    upload de imagem

--- mordomo/programas/relatorios/sql_server_relatorio_campanha.py
+++ mordomo/programas/relatorios/sql_server_relatorio_campanha.py
@@ -56,6 +56,17 @@ ORDER BY CODID
 '''
 data = pd.read_sql(comando, conexao)
 
+# Cria nova coluca com os valores da coluna SKU
+data['SKU_MESCLADO'] = data['SKU']
+
+# Preenche todos os valores vazios na coluna SKUVARIACAO_MASTER com o valor da mesma linha da coluna SKU
+data['SKUVARIACAO_MASTER'].fillna(data['SKU'], inplace=True)
+linhas_vazias = data.loc[data['SKUVARIACAO_MASTER'] == '']
+data.loc[linhas_vazias.index, 'SKUVARIACAO_MASTER'] = linhas_vazias['SKU']
+
+# Passa para a coluna SKU_MESCLADO os valores de SKUVARIACAO_MASTER (ML, Shopee, B2W, Tray, Decathlon)
+data.loc[data['ORIGEM_ID'].isin([8,9,10,11,12,13,17,18,19,32,33]), 'SKU_MESCLADO'] = data['SKUVARIACAO_MASTER']
+
 # Juntar código pai
 comando = '''
 SELECT COD_INTERNO AS PAI_COD_INTERNO, CODID AS PAI FROM MATERIAIS
@@ -79,7 +90,7 @@ data = pd.merge(data, data_categoria_magalu_tmp, on=['DESCRICAON02', 'API'], how
 data.rename(columns = {'DESCRICAON02':'CATEGORIAS'}, inplace=True)
 
 comando = '''
-SELECT A.PEDIDO, A.COD_INTERNO, A.QUANT, A.COD_PEDIDO AS SKU, A.EDICAO AS SKU2, B.ORIGEM,B.DATA
+SELECT A.PEDIDO, A.COD_INTERNO, A.QUANT, A.COD_PEDIDO AS SKU, A.EDICAO AS SKU2, B.ORIGEM AS ORIGEM_ID, B.DATA
 FROM PEDIDO_MATERIAIS_ITENS_CLIENTE A
 LEFT JOIN PEDIDO_MATERIAIS_CLIENTE B
 ON A.PEDIDO = B.PEDIDO
@@ -97,7 +108,7 @@ for i in range(len(data_h_aux)):
     quantidade = int(data_h_aux['QUANT'].iloc[i])
     sku = data_h_aux['SKU'].iloc[i]
     sku2 = data_h_aux['SKU2'].iloc[i]
-    origem = data_h_aux['ORIGEM'].iloc[i]
+    origem = data_h_aux['ORIGEM_ID'].iloc[i]
     data_venda = data_h_aux['DATA'].iloc[i]
     for j in range(quantidade):
         row1 = pd.Series([cod_interno, 1, sku, sku2, origem, data_venda], index=data_h.columns)
@@ -120,10 +131,19 @@ data_h_90.drop(['SKU', 'DATA'], axis=1, inplace=True)
 data_completo = pd.merge(data, data_h_30, on=['COD_INTERNO'], how='left')
 data_completo = pd.merge(data_completo, data_h_90, on=['COD_INTERNO'], how='left')
 
+# Colocando todos valores da coluna SKU2 na coluna SKU da Dafiti
+data_h.loc[data_h['ORIGEM_ID'].isin([5,6,7]), 'SKU'] = data_h['SKU2'].fillna(data_h['SKU'])
 
 # VENDAS MARKETPLACE (SKU ANÚNCIO)
 # Fazendo o Groupby de 90 e 30 dias
+data_h_30_mktp = data_h[(data_h['DATA'] >= date_30)]
+data_h_30_mktp['SKU_MESCLADO'] = data_h_30_mktp.apply(lambda x: x['SKU'] if pd.isnull(x['SKU2']) else x['SKU2'], axis=1)
+data_h_30_mktp.drop(['SKU', 'SKU2', 'DATA'], axis=1, inplace=True)
+data_h_30_mktp = data_h_30_mktp.groupby(['SKU_MESCLADO','ORIGEM_ID']).count()
+data_h_30_mktp = data_h_30_mktp.reset_index()
+data_h_30_mktp.drop(columns=['COD_INTERNO'], axis=1, inplace=True)
 
+data_h = pd.merge(data_h, data_h_30_mktp[['ORIGEM_ID', 'SKU_MESCLADO', 'QUANT']], on=['ORIGEM_ID', 'SKU_MESCLADO'], how='left')
 
 # Renomeando colunas
 data_completo = data_completo.rename(columns={'QUANT_x': '30_ATON', 'QUANT_y': '90_ATON', 'VLR_SITE1': 'PRECO_DE', 'VLR_SITE2': 'PRECO_POR'})
@@ -137,11 +157,12 @@ data_completo['90_MKTP'] = 'MANUTENCAO'
 
 data_completo['HORARIO'] = datetime.now()
 
-data = data_completo[['CODID', 'COD_INTERNO', 'PAI_COD_INTERNO', 'SKU', 'SKUVARIACAO_MASTER',
-                      'PRODMKTP_ID', 'DESCRICAO', 'GRUPO', 'VLR_CUSTO', 'PESO',
-                      'ESTOQUE', '30_ATON', '90_ATON', '30_MKTP', '90_MKTP','ORIGEM_NOME', 'CATEGORIAS', 'PRODUTO_TIPO',
-                      'COMPRIMENTO', 'LARGURA', 'ALTURA', 'TIPO_ANUNCIO', 'CATEG_ID', 'CATEG_NOME',
-                      'PRECO_DE', 'PRECO_POR', 'HORARIO']]
+data = data_completo
+# data = data_completo[['CODID', 'COD_INTERNO', 'PAI_COD_INTERNO', 'SKU', 'SKUVARIACAO_MASTER',
+#                       'PRODMKTP_ID', 'DESCRICAO', 'GRUPO', 'VLR_CUSTO', 'PESO',
+#                       'ESTOQUE', '30_ATON', '90_ATON', '30_MKTP', '90_MKTP','ORIGEM_NOME', 'CATEGORIAS', 'PRODUTO_TIPO',
+#                       'COMPRIMENTO', 'LARGURA', 'ALTURA', 'TIPO_ANUNCIO', 'CATEG_ID', 'CATEG_NOME',
+#                       'PRECO_DE', 'PRECO_POR', 'HORARIO']]
 
 # Removendo espaços em branco
 data['COD_INTERNO'] = data['COD_INTERNO'].str.strip()
